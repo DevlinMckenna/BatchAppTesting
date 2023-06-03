7eb4df28c506ef53ce528c96fa55556de351e4d1
DevlinMckenna <61163057+DevlinMckenna@users.noreply.github.com>
2023-06-03 14:53:04 -0400

    Update BatchDraft0.bat
    Fixed a bug with the clipboard feature of the script

--- BatchDraft0.bat
+++ BatchDraft0.bat
@@ -5,12 +5,12 @@ set /p input1= Enter User report:
 set /p input2= Enter Initial state of room:  
 set /p input3= Enter work done:
 set /p input4= Enter Resolution status
-echo Fielded a call from room: %input0%,User reported that %input1% Initially when tech arrived on Site tech observed %input2%, 
-echo Tech executed %input3%, After the work was finished the issue was, %input4% 
 
-@echo off
-(echo | set /p= %input0% ) |clip
+set message= Fielded a call from room: %input0%,User reported that %input1% Initially when tech arrived on Site tech observed %input2%, executed %input3%, After the work was finished the issue was, %input4% 
 
+echo %message%
 
+(echo %message%) | clip
 
+echo the above text is now in your clipboard.
 pause
